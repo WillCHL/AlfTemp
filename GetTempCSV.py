80c440173c3a80648b823a92de91db8e27f6f4b9
willhl <will.hore-lacy@arrb.com.au>
2016-05-24 14:12:34 +1000

    Updated GetTempCSV.py to limit attempts when sensor can't be found.

--- GetTempCSV.py
+++ GetTempCSV.py
@@ -2,16 +2,22 @@
 import time
 import os.path
 from datetime import datetime #, deltatime
+import numpy as np
 
-#Set sleep time in seconds
-sleeptime=50
+#Max attempts variable
+maxattempts=3
 
 #Set days per file
 ndays=7
 
+#Base File Name
+basefname="Temp1.csv"
+
 # ID's: waterproof probe, chip
 sensorids = ["28-01159070bcff", "28-0215637a92ff"]
 fullpath = "/home/pi/Documents/TempLog/data/"
+# filenames = os.listdir(fullpath) Used if doing multi dy files
+# filenames = [i for i in filenames if i.endswith(basefname)]
 
 #function to create new file
 def newFile(filenm):
@@ -19,51 +25,49 @@ def newFile(filenm):
 	headers.extend(sensorids)
 	print headers
 	f=open(fullpath+filenm,'a')
-	f.write(', '.join(headers)+'\n')
+	f.write(','.join(headers)+'\n')
 	f.close()
 
 now=datetime.now()
-starttime=now
 
-writefile=now.strftime('%Y-%m-%d')+"Temp1.csv"
+writefile=now.strftime('%Y-%m-%d') + basefname
 
 # Write headers for new data file if file with todays date does not exist
 if not os.path.exists(fullpath + writefile):
         newFile(writefile)
 
 # Loop to collect data
-
-now=datetime.now()
-delta=now-starttime
-if delta.days>ndays:
-	writefile=now.strftime('%Y-%m-%d')+"Temp1.csv"
-	newFile(writefile)
-	starttime=now
-
 avgtemperatures = []
 for sensor in range(len(sensorids)):
 	temperatures = []
 	for polltime in range(0,3):
 			text = '';
+			attempts=0
+			temperature=[]
 			while text.split("\n")[0].find("YES") == -1:
-					tfile = open("/sys/bus/w1/devices/"+ sensorids[sensor] +"/w1_slave")
-					text = tfile.read()
-					tfile.close()
+					if attempts==maxattempts:
+						temperature=np.nan
+						break
+					try:
+						tfile = open("/sys/bus/w1/devices/"+ sensorids[sensor] +"/w1_slave")
+						text = tfile.read()
+						tfile.close()
+					except IOError:
+						print 'Sensor not found: ', sensorids[sensor]
 					time.sleep(1)
+					attempts += 1
  
-			secondline = text.split("\n")[1]
-			temperaturedata = secondline.split(" ")[9]
-			temperature = float(temperaturedata[2:])
+			if not np.isnan(temperature):
+				secondline = text.split("\n")[1]
+				temperaturedata = secondline.split(" ")[9]
+				temperature = float(temperaturedata[2:])
 			temperatures.append(temperature / 1000)
 	
 	avgtemperatures.append("%.2f" % round(sum(temperatures) / float(len(temperatures)),2))
 
 # Generate list of time and temperature values and write to file
 
-#print "Avg Temp: ",avgtemperatures
 timevalue=time.asctime(time.localtime(time.time()))
-# print timevalue
-# output_vec = [timevalue]
 output_vec = [now.strftime('%Y-%m-%d'),now.strftime('%H:%M:%S')]
 output_vec.extend(avgtemperatures)
 print output_vec
@@ -75,7 +79,7 @@ for item in output_vec:
 		char=""
 		c=0
 	else:
-		char=", "
+		char=","
 	f.write(char+str(item))
 f.write("\n")
 f.close()
